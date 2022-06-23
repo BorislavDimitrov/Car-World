e9645473407666db8c42ad2030d5333be5887362
BorislavDimitrov <bdimitorv@gmail.com>
2022-06-23 12:04:45 +0300

    Create post db model

---
+++ Data/CarWorld.Data.Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using CarWorld.Data.Common.Models;
+using System.Collections.Generic;
+
+namespace CarWorld.Data.Models
+{
+    public class Category : BaseDeletableModel<int>
+    {
+        public Category()
+        {
+            this.Posts = new HashSet<Post>();
+        }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public IEnumerable<Post> Posts { get; set; }
+    }
+}
---
+++ Data/CarWorld.Data.Models/Post.cs
@@ -0,0 +1,25 @@
+﻿using CarWorld.Data.Common.Models;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarWorld.Data.Models
+{
+    public class Post : BaseDeletableModel<int>
+    {
+       
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string CreatorId { get; set; }
+
+        [ForeignKey(nameof(CreatorId))]
+        public ApplicationUser Creator { get; set; }
+
+        public int CategoryId { get; set; }
+
+        [ForeignKey(nameof(CategoryId))]
+        public Category Category { get; set; }
+    }
+}
