5d54dce290c51a16c1268866eb31c5ba98f255a7
BorislavDimitrov <bdimitorv@gmail.com>
2023-01-27 23:34:35 +0200

    Create method for insertion in cart

--- Services/CarWorld.Services/CartsService.cs
+++ Services/CarWorld.Services/CartsService.cs
@@ -1,6 +1,7 @@
 ﻿using CarWorld.Data.Common.Repositories;
 using CarWorld.Data.Models;
 using CarWorld.Services.Contracts;
+using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 
 namespace CarWorld.Services
@@ -8,10 +9,14 @@ namespace CarWorld.Services
     public class CartsService : ICartsService
     {
         private readonly IRepository<Cart> cartsRepo;
+        private readonly IRepository<Product> productsRepo;
+        private readonly IRepository<Item> itemsRepo;
 
-        public CartsService(IRepository<Cart> cartsRepo)
+        public CartsService(IRepository<Cart> cartsRepo,
+            IRepository<Product> productsRepo)
         {
             this.cartsRepo = cartsRepo;
+            this.productsRepo = productsRepo;
         }
 
         public async Task CreateAsync(string userId)
@@ -23,6 +28,36 @@ namespace CarWorld.Services
 
             await cartsRepo.AddAsync(cart);
             await cartsRepo.SaveChangesAsync();
+        }
+
+        public async Task AddProductToCartAsync(int id, string userId, int quanity)
+        {
+            var cart = await cartsRepo.All()
+                .FirstOrDefaultAsync(x => x.UserId == userId);
+
+            var product = await productsRepo.All()
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            var item = await CreateItemAsync(product, cart.Id, quanity);
+
+            cart.Items.Add(item);
+
+            await cartsRepo.SaveChangesAsync();
+        }
+
+        private async Task<Item> CreateItemAsync(Product product, int cartId, int quanity)
+        {
+            var item = new Item
+            {
+                CartId = cartId,
+                Product = product,
+                ProductId = cartId,
+                Quanity = quanity,
+            };
+
+            await itemsRepo.AddAsync(item);
+            await itemsRepo.SaveChangesAsync();
+            return item;
         }
     }
 }
--- Services/CarWorld.Services/Contracts/ICartsService.cs
+++ Services/CarWorld.Services/Contracts/ICartsService.cs
@@ -5,5 +5,7 @@ namespace CarWorld.Services.Contracts
     public interface ICartsService
     {
         Task CreateAsync(string userId);
+
+        Task AddProductToCartAsync(int id, string userId, int quanity);
     }
 }
