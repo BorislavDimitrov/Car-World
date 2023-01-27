1d2640a7e914b773faedec8f52c1876d07b4ef1a
BorislavDimitrov <bdimitorv@gmail.com>
2023-01-27 10:48:07 +0200

    Implement Create method of the cart

--- Services/CarWorld.Services/CartsService.cs
+++ Services/CarWorld.Services/CartsService.cs
@@ -1,6 +1,28 @@
-﻿namespace CarWorld.Services
+﻿using CarWorld.Data.Common.Repositories;
+using CarWorld.Data.Models;
+using CarWorld.Services.Contracts;
+using System.Threading.Tasks;
+
+namespace CarWorld.Services
 {
-    public class CartsService
+    public class CartsService : ICartsService
     {
+        private readonly IRepository<Cart> cartsRepo;
+
+        public CartsService(IRepository<Cart> cartsRepo)
+        {
+            this.cartsRepo = cartsRepo;
+        }
+
+        public async Task CreateAsync(string userId)
+        {
+            var cart = new Cart
+            {
+                UserId = userId,
+            };
+
+            await cartsRepo.AddAsync(cart);
+            await cartsRepo.SaveChangesAsync();
+        }
     }
 }
