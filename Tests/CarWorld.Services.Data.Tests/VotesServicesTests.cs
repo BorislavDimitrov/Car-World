7437179c10dc3c336f923cc953bee7b90edc488b
BorislavDimitrov <86628373+BorislavDimitrov@users.noreply.github.com>
2023-04-13 23:01:54 +0300

    Make a class to test votes service.

---
+++ Tests/CarWorld.Services.Data.Tests/VotesServicesTests.cs
@@ -0,0 +1,33 @@
+﻿using CarWorld.Data;
+using CarWorld.Data.Common.Repositories;
+using CarWorld.Data.Models;
+using CarWorld.Data.Repositories;
+using CarWorld.Services.Contracts;
+using CarWorld.Services.Mapping;
+using CarWorld.Web.ViewModels.Administration;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+
+namespace CarWorld.Services.Data.Tests
+{
+    public class VotesServicesTests
+    {
+        private DbContextOptionsBuilder<ApplicationDbContext> options;
+        private ApplicationDbContext dbContext;
+        private IRepository<Vote> votesRepository;
+        private IVotesService votesService;
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            this.options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            this.dbContext = new ApplicationDbContext(this.options.Options);
+            this.votesRepository = new EfRepository<Vote>(this.dbContext);
+            this.votesService = new VotesService(this.votesRepository);
+        }
+
+    }
+}
