c1b08619158b99394c52d79748a1afe9da95dd8e
BorislavDimitrov <86628373+BorislavDimitrov@users.noreply.github.com>
2023-04-27 17:11:19 +0300

    Create seeding method for car reports in unit test class

--- Tests/CarWorld.Services.Data.Tests/ReportsServiceTests.cs
+++ Tests/CarWorld.Services.Data.Tests/ReportsServiceTests.cs
@@ -1,9 +1,12 @@
 ﻿using CarWorld.Data;
 using CarWorld.Data.Models;
+using CarWorld.Data.Models.Enums;
 using CarWorld.Data.Repositories;
 using CarWorld.Services.Contracts;
 using CarWorld.Services.Mapping;
 using CarWorld.Web.ViewModels.Administration;
+using CarWorld.Web.ViewModels.Administration.CarReports;
+using CarWorld.Web.ViewModels.Reports;
 using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
 using System;
@@ -27,7 +30,22 @@ namespace CarWorld.Services.Data.Tests
             this.reportsRepository = new EfRepository<CarReport>(this.dbContext);
             this.reportsService = new ReportsService(this.reportsRepository);
 
-            AutoMapperConfig.RegisterMappings(typeof(MakeInListViewModel).Assembly);
+            AutoMapperConfig.RegisterMappings(typeof(CarReportsInListViewModel).Assembly);
+        }
+
+        private async Task ReportsSeedingAsync(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                await this.reportsService.CreateCarReportAsync(new CarReportInputModel
+                {
+                    CarId = i,
+                    Description = "Description",
+                    Title = "Title",
+                    ReporterId = i.ToString(),
+                    ReportType = ReportType.Scam,
+                });
+            }
         }
     }
 }
