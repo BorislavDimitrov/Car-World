ce54f52fd31721f2adeab93c4037ae0fa6327f49
BorislavDimitrov <86628373+BorislavDimitrov@users.noreply.github.com>
2023-05-04 17:59:53 +0300

    delete product model

--- Data/CarWorld.Data/ApplicationDbContext.cs
+++ Data/CarWorld.Data/ApplicationDbContext.cs
@@ -44,8 +44,6 @@
 
         public DbSet<Vote> Votes { get; set; }
 
-        public DbSet<Product> Products { get; set; }
-
         public override int SaveChanges() => this.SaveChanges(true);
 
         public override int SaveChanges(bool acceptAllChangesOnSuccess)
